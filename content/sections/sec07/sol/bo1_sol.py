690f9077c55d06c435e634d96df21fc826a83e1d
Marios Mattheakis <mariosmattheakis@inabook.local>
2021-04-16 10:06:08 -0400

    sec7_updated

--- content/sections/sec07/sol/bo1_sol.py
+++
@@ -1,7 +0,0 @@
-#solution
-class Analytical_KL:
-    def __init__(self, Lambda):
-        self.Lambda = Lambda
-    def __call__(self, mu, logvariance):
-        lossval = np.sum(-0.5*(1 + logvariance - mu**2 - logvariance),axis=-1)
-        return lossval 
